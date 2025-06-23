28564c341f9e44c8f75b05ddcb095c073c8e4d3b
Vladislav <vladislavkhramov.r@gmail.com>
2025-06-23 11:29:50 +0300

    implemented a working build

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,6 +1,8 @@
-'use server'
 import { ProjectWidget } from '@/widgets/projectWidget/ui/ProjectWidget'
 import { getUserProjects } from '@/app/api/getProject/getUserProjects'
+
+export const dynamic = 'force-dynamic'
+
 
 export default async function Home() {
     const projects = await getUserProjects()
